cf6cfb1236cff0f72bb5ea9f98cf9d19321c74bb
John Huseinovic <john@huseinovic.net>
2026-07-14 11:34:22 +0000

    Support for REDISCLOUD_URL environment variable

--- snappass/main.py
+++ snappass/main.py
@@ -39,16 +39,21 @@ babel = Babel(app, locale_selector=get_locale)
 # Initialize Redis
 if os.environ.get('MOCK_REDIS'):
     from fakeredis import FakeStrictRedis
-
     redis_client = FakeStrictRedis()
+
 elif os.environ.get('REDIS_URL'):
     redis_client = redis.StrictRedis.from_url(os.environ.get('REDIS_URL'))
+
+elif os.environ.get('REDISCLOUD_URL'):
+    redis_client = redis.StrictRedis.from_url(os.environ.get('REDISCLOUD_URL'))
+
 else:
     redis_host = os.environ.get('REDIS_HOST', 'localhost')
     redis_port = os.environ.get('REDIS_PORT', 6379)
     redis_db = os.environ.get('SNAPPASS_REDIS_DB', 0)
     redis_client = redis.StrictRedis(
         host=redis_host, port=redis_port, db=redis_db)
+        
 REDIS_PREFIX = os.environ.get('REDIS_PREFIX', 'snappass')
 
 TIME_CONVERSION = {'two weeks': 1209600, 'week': 604800, 'day': 86400,
